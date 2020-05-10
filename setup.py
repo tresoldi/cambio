a6c2966767f12030389a2522b9cf996c62313e2c
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-05-10 18:22:36 +0200

    Reorganize setup.py

--- setup.py
+++ setup.py
@@ -9,32 +9,32 @@ LOCAL_PATH = pathlib.Path(__file__).parent
 README_FILE = (LOCAL_PATH / "README.md").read_text()
 
 # Load requirements, so they are listed in a single place
-with open('requirements.txt') as fp:
+with open("requirements.txt") as fp:
     install_requires = [dep.strip() for dep in fp.readlines()]
 
 # This call to setup() does all the work
 setup(
-    name="alteruphono",
-    version="0.4",
-    description="Library for dealing with sound changes",
-    long_description=README_FILE,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tresoldi/alteruphono",
-    author="Tiago Tresoldi",
     author_email="tresoldi@shh.mpg.de",
-    license="MIT",
+    author="Tiago Tresoldi",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries",
     ],
-    packages=["alteruphono", "resources"],
-    keywords=["sound change", "phonology", "phonetics", "Lautwandel"],
+    description="Library for dealing with sound changes",
+    entry_points={"console_scripts": ["alteruphono=alteruphono.__main__:main"]},
     include_package_data=True,
     install_requires=install_requires,
-    entry_points={"console_scripts": ["alteruphono=alteruphono.__main__:main"]},
+    keywords=["sound change", "phonology", "phonetics", "Lautwandel"],
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=README_FILE,
+    name="alteruphono",
+    packages=["alteruphono", "resources"],
     test_suite="tests",
     tests_require=[],
+    url="https://github.com/tresoldi/alteruphono",
+    version="0.4",
     zip_safe=False,
 )
